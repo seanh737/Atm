19052a1f22d38ce50994b0763e5c0112f77f8191
Sean Hsieh <seanh737@gmail.com>
2017-11-28 11:59:08 +0800

    Login system

--- app/src/main/java/com/sean/atm/LoginActivity.java
+++ app/src/main/java/com/sean/atm/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.sean.atm;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,4 +14,24 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+    public void login(View view){
+        EditText edUserid = (EditText) findViewById(R.id.userid);
+        EditText edpasswd = (EditText) findViewById(R.id.passwd);
+        String userid = edUserid.getText().toString();
+        String passwd = edpasswd.getText().toString();
+        if(userid.equals("jack") && passwd.equals("12345")){
+            Toast.makeText(this , "登入成功" , Toast.LENGTH_LONG).show();
+            finish();
+        }else{
+            new AlertDialog.Builder(this)
+                    .setTitle("錯誤")
+                    .setMessage("登入失敗")
+                    .setPositiveButton("OK",null)
+                    .show();
+        }
+    }
+    public  void cancel(View view){
+
+    }
+
 }
