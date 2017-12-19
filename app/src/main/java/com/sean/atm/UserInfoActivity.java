93fa8768ff047f978f5c4098571e1d184d4290ee
Sean Hsieh <seanh737@gmail.com>
2017-12-19 11:43:15 +0800

    12/19 spinner practice

--- app/src/main/java/com/sean/atm/UserInfoActivity.java
+++ app/src/main/java/com/sean/atm/UserInfoActivity.java
@@ -2,14 +2,21 @@ package com.sean.atm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class UserInfoActivity extends AppCompatActivity {
 
 
     private EditText edName;
     private EditText edPhoneNum;
+    private Spinner age;
+    private String TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +30,19 @@ public class UserInfoActivity extends AppCompatActivity {
                 .getString("PHONENUM" ,"");
         edName.setText(name);
         edPhoneNum.setText(phonenum);
+        // 12/19 下拉式捲軸 sipnner
+        age = (Spinner) findViewById(R.id.age);
+//        ArrayList<String> data = new ArrayList<>();//能運用陣列   以下兩種方法
+//        for (int i = 15; i <= 40 ; i++){
+//            data.add(i+"");
+//        }
+//        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, data);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.aaa, android.R.layout.simple_list_item_1);  //利用values>>strings.xml  來儲存資料
+        age.setAdapter(adapter);
 
     }
     public void ok(View view){
-
+        Log.d(TAG, "ok" +age.getSelectedItem().toString());
         String name = edName.getText().toString();
         String phonenum = edPhoneNum.getText().toString();
         getIntent().putExtra("INFO_NAME",name);
